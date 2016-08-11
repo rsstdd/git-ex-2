8895d0c2bdfb1536ecefff07faa51edc05a75a84
rsstdd <rosstod@gmail.com>
2016-08-11 10:30:53 -0700

    added additional if stmt

--- if.js
+++ if.js
@@ -6,3 +6,11 @@ if (total >= 100) {
 
 //Display the total to the user.
 console.log('Your total is: $' + total.toFixed(2));
+
+if ( 1 + 1 ===2 ) {
+  console.log('Arithmetic is the best');
+}
+
+if ( 1  + 1 !== 2) {
+  console.log('Math is broken');
+}
