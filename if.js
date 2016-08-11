9c08ab90521da147d2257fd05f9369b31d4fa74e
rsstdd <rosstod@gmail.com>
2016-08-11 10:53:32 -0700

    added if/else stmt

--- if.js
+++ if.js
@@ -14,3 +14,10 @@ if ( 1 + 1 ===2 ) {
 if ( 1  + 1 !== 2) {
   console.log('Math is broken');
 }
+
+
+if ( 1 + 1 === 2) {
+  console.log('Arithmetic is the best');
+} else {
+  console.log('math is broken');
+}
