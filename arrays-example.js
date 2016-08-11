cbb7992ca4a9206ef0bdb0df5130c34a86986b88
rsstdd <rosstod@gmail.com>
2016-08-11 16:47:14 -0700

    added arrays-examples and improved the doubles function

--- arrays-example.js
+++ arrays-example.js
@@ -1,1 +1,12 @@
-var arrays
+var myArray = ['Elie', 'Janey', 'Matt', 'Parker', 'Tim'];
+
+console.log(myArray);
+
+var petNames = ['Nilo', 'Loki', 'Dino', 'Artemis'];
+
+console.log(petNames);
+
+myArray[-1];
+//myArray[100];
+
+console.log(myArray.length)
